8a7d95f8cba2d0bac8f1197843a4c3b0cd1d4e24
Ashish Anand <32809264+ashish1779@users.noreply.github.com>
2019-11-11 11:05:57 +0530

    Update PID_PWM_Generator.vhd

--- PID_PWM_Generator.vhd
+++ PID_PWM_Generator.vhd
@@ -3,21 +3,21 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 ENTITY PID IS
 generic(
-			N                     : integer := 12);      -- number of bit of PWM counter
-PORT (
+		N                     : integer := 12);      -- number of bit of PWM counter
+PORT   (
 		
 		i_rstb                      : in  std_logic;
 		i_sync_reset                : in  std_logic;
 		i_pwm_module                : in  std_logic_vector(N-1 downto 0);  -- PWM Freq  = clock freq/ (i_pwm_module+1); max value = 2^N-1
-	    --i_pwm_width                 : in  std_logic_vector(N-1 downto 0);  -- PWM width = (others=>0)=> OFF; i_pwm_module => MAX ON 
-	   o_pwm                       : out std_logic;
+	        --i_pwm_width                 : in  std_logic_vector(N-1 downto 0);  -- PWM width = (others=>0)=> OFF; i_pwm_module => MAX ON 
+	        o_pwm                       : out std_logic;
 		kp_sw          : IN std_logic; --determines if p term is needed
 		ki_sw          : IN std_logic; --determines if i term is needed
 		kd_sw          : IN std_logic; --determines if d term is needed
 		SetVal         : IN std_logic_vector(11 DOWNTO 0); --user input reference
 		adc_data       : IN std_logic_vector(11 DOWNTO 0); --feedbac value from sensor
 		on_off_switch  : IN std_logic; --determines if controller is active
-	   --		output         : OUT std_logic_vector(11 DOWNTO 0); --output of controller
+	        --output         : OUT std_logic_vector(11 DOWNTO 0); --output of controller
 		clk            : IN STD_LOGIC );
 END PID;
 
@@ -29,18 +29,18 @@ ARCHITECTURE Behavioral OF PID IS
 	signal w_tc_pwm_counter                      : std_logic;
 	signal i_pwm_width                           : std_logic_vector(N-1 downto 0); 
 	
-	CONSTANT con_Kp : INTEGER := 1; --proportional constant
-	CONSTANT con_kp_den : INTEGER := 2;
-	CONSTANT con_Kd : INTEGER := 1; --differential constant
-	CONSTANT con_kd_den : INTEGER := 100;
-	CONSTANT con_Ki : INTEGER := 1; --integral constant
-	CONSTANT con_ki_den : INTEGER := 10;
+	CONSTANT con_Kp 	: INTEGER := 1; --proportional constant
+	CONSTANT con_kp_den 	: INTEGER := 2;
+	CONSTANT con_Kd 	: INTEGER := 1; --differential constant
+	CONSTANT con_kd_den 	: INTEGER := 100;
+	CONSTANT con_Ki 	: INTEGER := 1; --integral constant
+	CONSTANT con_ki_den 	: INTEGER := 10;
 	SIGNAL Error, Error_difference, error_sum, old_error : INTEGER := 0; --store values for controller
-	SIGNAL p, i, d : INTEGER := 0; 													--Contain the proportional, derivative and integral errors respectively
+	SIGNAL p, i, d 		: INTEGER := 0; 													--Contain the proportional, derivative and integral errors respectively
 	SIGNAL output_loaded, output_saturation_buffer : INTEGER := 0;			--allows to check if output is within range
-	SIGNAL old_adc : std_logic_vector(11 DOWNTO 0);               			--stores old adc value
+	SIGNAL old_adc 		: std_logic_vector(11 DOWNTO 0);               			--stores old adc value
 	CONSTANT divider_for_time : INTEGER := 1;                    			--stores the time in which the controller acts over example a value of 100 would be equalt to 10ms so 1/divider_for_time = sampling_period
-   SIGNAL output : std_logic_vector(11 DOWNTO 0);
+        SIGNAL output 		: std_logic_vector(11 DOWNTO 0);
 BEGIN
 
 	w_tc_pwm_counter  <= '0' when(r_pwm_counter<r_max_count) else '1';  -- use to strobe new word
@@ -96,13 +96,16 @@ END CASE;
 END LOOP;
 END IF;
 END IF;
- i_pwm_width <= output ;
+
+i_pwm_width <= output ;
+
 if(i_rstb='0') then
     r_max_count     <= (others=>'0');
     r_pwm_width     <= (others=>'0');
     r_pwm_counter   <= (others=>'0');
     o_pwm           <= '0';
-  elsif(rising_edge(clk)) then
+
+elsif(rising_edge(clk)) then
     r_max_count     <= unsigned(i_pwm_module);
     if(i_sync_reset='1') then
       r_pwm_width     <= unsigned(i_pwm_width);
@@ -127,9 +130,7 @@ if(i_rstb='0') then
         r_pwm_counter   <= r_pwm_counter + 1;
       end if;
     end if;
-  end if;
+end if;
   
-  
-
 END PROCESS; --end of process
 END Behavioral; --end of Architecture
